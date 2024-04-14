c4f8c53e2b1958c6d871ed6f3ec8681e00a73bb8
MaxenV <maxen2001@gmail.com>
2024-04-14 14:34:51 +0200

    City Location intent, config actions
    Create action city location with default response
    Config to run actions
    Create intent city_location
    Create entity and slot city
    Create rule Say where city is located

--- app/actions/actions.py
+++ app/actions/actions.py
@@ -7,21 +7,25 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionCityLocation(Action):
+
+    def name(self) -> Text:
+        return "action_city_location"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="ActionCityLocation running successfully")
+        dispatcher.utter_message(text="city name is: " + tracker.get_slot("city"))
+
+        return []

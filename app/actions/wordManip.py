341fe318dd95638f2e091cd8dd917653c75998f2
MaxenV <maxen2001@gmail.com>
2024-04-16 12:13:28 +0200

    Crate wordManipulation helper with morfeusz2
    to_nominative change diffrent ways to say city name
    to nominatice type:
    Lublinie, Lublina -> Lublin
    Warszawie, Warszawy -> Warszawa

---
+++ app/actions/wordManip.py
@@ -0,0 +1,13 @@
+import morfeusz2
+
+
+class WordManip:
+    def __init__(self):
+        self.morf = morfeusz2.Morfeusz()
+
+    def to_nominative(self, word):
+        analysis = self.morf.analyse(word[0].upper() + word[1:].lower())
+        for i in analysis:
+            if "nazwa_geograficzna" in i[-1][-2]:
+                return i[-1][1].split(":")[0]
+        return None

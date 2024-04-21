55fa8009437527c585b067a2f61adbcd8b5d0290
MaxenV <maxen2001@gmail.com>
2024-04-21 23:09:09 +0200

    Set keys and create service instance

--- app/actions/__init__.py
+++ app/actions/__init__.py
@@ -1,6 +1,7 @@
 import os
 from .GUSService import GUSService
 from .wordManip import WordManip
+from .GoogleService import GoogleService
 
 # Get the GUSkey from environment variables
 GUSkey = os.getenv("GUSkey")
@@ -13,6 +14,21 @@ else:
     print("GUSkey is set in environment variables.")
     gusService = GUSService(apiKey=GUSkey)
 
+# Check if Google API keys are set
+GoogledeveloperKey = os.getenv("GoogledeveloperKey")
+GoogleCX = os.getenv("GoogleCX")
+
+if (
+    GoogledeveloperKey is None
+    or GoogleCX is None
+    or GoogledeveloperKey == "<your_api_key>"
+    or GoogleCX == "<your_cx>"
+):
+    print("Google API keys are not set in environment variables.")
+    googleService = None
+else:
+    print("Google API keys are set in environment variables.")
+    googleService = GoogleService(GoogledeveloperKey, GoogleCX)
 
 # Create an instance of WordManip
 wordManip = WordManip()
